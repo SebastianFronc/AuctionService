2a1939f4b879be0283598fa8b5bdd4820d060a44
Sebastian Fronc <fronc.sebastian@gmail.com>
2019-08-04 11:53:31 +0200

    Initial commit

---
+++ src/test/java/com/seba/AuctionService/AuctionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.seba.AuctionService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuctionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
