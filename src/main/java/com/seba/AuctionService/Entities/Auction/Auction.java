4e9184fe579cb8fd355c2503734c9a15ac620740
Sebastian Fronc <fronc.sebastian@gmail.com>
2019-08-05 22:55:21 +0200

    Added auction class

--- src/main/java/com/seba/AuctionService/Entities/Auction/Auction.java
+++ src/main/java/com/seba/AuctionService/Entities/Auction/Auction.java
@@ -1,4 +1,50 @@
 package com.seba.AuctionService.Entities.Auction;
 
+import com.seba.AuctionService.Entities.User.User;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "auction")
 public class Auction {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "actual_price")
+    private BigDecimal actualPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "actual_winner_id")
+    private User actualWinner;
+
+    public Auction() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getActualPrice() {
+        return actualPrice;
+    }
+
+    public void setActualPrice(BigDecimal actualPrice) {
+        this.actualPrice = actualPrice;
+    }
+
+    public User getActualWinner() {
+        return actualWinner;
+    }
+
+    public void setActualWinner(User actualWinner) {
+        this.actualWinner = actualWinner;
+    }
 }
